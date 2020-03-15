4d77417c53f52fc0e5df1819825a5d1c6a93267d
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-03-15 21:10:00 +0100

    Removed comments from setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
-
 setup(
     name='regn',  # Required
     version='0.1',  # Required
@@ -17,9 +14,8 @@ setup(
     url='https://github.com/simonpf/regn',  # Optional
     author='Simon Pfreundschuh, Inderpreet Kaur, Patrick Eriksson',  # Optional
     author_email='simon.pfreundschuh@chalmers.se',  # Optional
-    packages=["regn"]
+    packages=["regn"],
     python_requires='>=3.6',
-    install_requires=['typhon>0.8'],  # Optional
     project_urls={  # Optional
         'Source': 'https://github.com/simonpf/regn/',
     })
