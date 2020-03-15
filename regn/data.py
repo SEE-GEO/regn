693a9351f54b78c3ba6d7515d8505ea443b2aec4
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-03-15 22:47:24 +0100

    Added script to extract training data.

--- regn/data.py
+++ regn/data.py
@@ -1,4 +1,6 @@
-import numpy.np
+import numpy as np
+import glob
+import os
 import netCDF4
 
 types =  [('nx', 'i4'), ('ny', 'i4')]
@@ -36,7 +38,7 @@ def check_sample(data):
     """
     return all([data[i] > 0 and data[i] < 1000 for i in range(13, 26)])
 
-def write_to_file(file, data, subsampling = 0.01):
+def write_to_file(file, data, samples = -1):
     """
     Write data to NetCDF file.
 
@@ -44,15 +46,15 @@ def write_to_file(file, data, subsampling = 0.01):
         file: File handle to the output file.
         data: numpy memmap object pointing to the binary data
     """
-    v_tbs = file.variables["tbs"]
-    v_lats = file.variables["lats"]
-    v_lons = file.variables["lons"]
-    v_sfccode = file.variables["sfccode"]
+    v_tbs = file.variables["brightness_temperature"]
+    v_lats = file.variables["latitude"]
+    v_lons = file.variables["longitude"]
+    v_sfccode = file.variables["surface_type"]
     v_tcwv = file.variables["tcwv"]
-    v_t2m = file.variables["T2m"]
+    v_t2m = file.variables["t2m"]
     v_surf_precip = file.variables["surface_precipitation"]
-    v_tbs_min = file.variables["tbs_min"]
-    v_tbs_max = file.variables["tbs_max"]
+    v_tbs_min = file.variables["bt_min"]
+    v_tbs_max = file.variables["bt_max"]
 
     v_year = file.variables["year"]
     v_month = file.variables["month"]
@@ -62,20 +64,19 @@ def write_to_file(file, data, subsampling = 0.01):
     v_second = file.variables["second"]
 
     i = file.dimensions["samples"].size
-    for d in data:
 
-        if np.random.rand() > subsampling:
-            continue
-
-        # Check if sample is valid.
-        if not check_sample(d):
-            continue
+    inds = np.random.permutation(np.arange(data.shape[0]))
+    j = 0
+    for _ in range(samples):
+        while not check_sample(data[inds[j]]):
+            j = j+1
+        d = data[inds[j]]
 
         v_year[i] = d[2]
         v_month[i] = d[3]
         v_day[i] = d[4]
         v_hour[i] = d[5]
-        v_year[i] = d[6]
+        v_minute[i] = d[6]
         v_second[i] = d[7]
 
         v_lats[i] = d[8]
@@ -83,12 +84,13 @@ def write_to_file(file, data, subsampling = 0.01):
         v_sfccode[i] = d[10]
         v_tcwv[i] = d[11]
         v_t2m[i] = d[12]
-        for j in range(13):
-            v_tbs_min[j] = np.minimum(v_tbs_min[j], d[13 + j])
-            v_tbs_max[j] = np.maximum(v_tbs_max[j], d[13 + j])
-            v_tbs[i, j] = d[13 + j]
+        for k in range(13):
+            v_tbs_min[k] = np.minimum(v_tbs_min[k], d[13 + k])
+            v_tbs_max[k] = np.maximum(v_tbs_max[k], d[13 + k])
+            v_tbs[i, k] = d[13 + k]
         v_surf_precip[i] = d[26]
         i += 1
+        j += 1
 
 def create_output_file(path):
     """
@@ -103,14 +105,14 @@ def create_output_file(path):
     file = netCDF4.Dataset(path, "w")
     file.createDimension("channels", size = 13)
     file.createDimension("samples", size = None) #unlimited dimensions
-    file.createVariable("tbs", "f4", dimensions = ("samples", "channels"))
-    file.createVariable("tbs_min", "f4", dimensions = ("channels"))
-    file.createVariable("tbs_max", "f4", dimensions = ("channels"))
-    file.createVariable("lats", "f4", dimensions = ("samples",))
-    file.createVariable("lons", "f4", dimensions = ("samples",))
-    file.createVariable("sfccode", "f4", dimensions = ("samples",))
+    file.createVariable("brightness_temperature", "f4", dimensions = ("samples", "channels"))
+    file.createVariable("bt_min", "f4", dimensions = ("channels"))
+    file.createVariable("bt_max", "f4", dimensions = ("channels"))
+    file.createVariable("latitude", "f4", dimensions = ("samples",))
+    file.createVariable("longitude", "f4", dimensions = ("samples",))
+    file.createVariable("surface_type", "f4", dimensions = ("samples",))
     file.createVariable("tcwv", "f4", dimensions = ("samples",))
-    file.createVariable("T2m", "f4", dimensions = ("samples",))
+    file.createVariable("t2m", "f4", dimensions = ("samples",))
     file.createVariable("surface_precipitation", "f4", dimensions = ("samples",))
     # Also include date.
     file.createVariable("year", "i4", dimensions = ("samples",))
@@ -120,23 +122,32 @@ def create_output_file(path):
     file.createVariable("minute", "i4", dimensions = ("samples",))
     file.createVariable("second", "i4", dimensions = ("samples",))
 
-    file["tbs_min"][:] = 1e30
-    file["tbs_max"][:] = 0.0
+    file["bt_min"][:] = 1e30
+    file["bt_max"][:] = 0.0
 
     return file
 
-def extract_data(base_path, file, subsampling = 0.01):
+def extract_data(base_path, y, m, d, samples, file):
     """
-    Extract training data from GPROF binary files for given year, month and day.
+    Extract training data from binary files for given year, month and day.
 
     Arguments:
+        base_path(str): Base directory containing the training data.
         year(int): The year from which to extract the training data.
         month(int): The month from which to extract the training data.
         day(int): The day for which to extract training data.
         file: File handle of the netCDF4 file into which to store the results.
     """
-    files = glob.glob(os.path.join(base_path, "**", "*.dat"))
-    for f in tqdm.tqdm(files):
-        with open(f, 'rb') as fn:
+    path = os.path.join(base_path,
+                        "{:02d}{:02d}".format(y, m),
+                        "*20{:02d}{:02d}{:02d}*.dat".format(y, m, d))
+    files = glob.glob(path)
+    if len(files) > 0:
+        samples_per_file = samples // len(files)
+        for i, f in enumerate(files):
+            n = samples_per_file
+            if i < samples % len(files):
+                n += 1
+            with open(f, 'rb') as fn:
                 data = read_file(fn)
-                write_to_file(f, data, subsampling = subsampling)
+                write_to_file(file, data, samples = n)
---
+++ scripts/extract_training_data.py
@@ -0,0 +1,40 @@
+import glob
+import os
+import numpy as np
+import tqdm
+from calendar import monthrange
+from regn.data import create_output_file, extract_data
+import argparse
+
+################################################################################
+# Command line arguments.
+################################################################################
+
+parser = argparse.ArgumentParser(description='Extract training data')
+parser.add_argument('input', metavar='input', type=str, nargs=1,
+                    help='Path to input data.')
+parser.add_argument('n_samples', metavar='n', type=int, nargs=1,
+                    help='How many samples to extract from each file.')
+parser.add_argument('output', metavar="output", type=str, nargs=1,
+                    help='Filename for output file.')
+
+args = parser.parse_args()
+data_path = args.input[0]
+samples = args.n_samples[0]
+output = args.output[0]
+
+################################################################################
+# Extract data.
+################################################################################
+
+months = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8]
+years = [14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15]
+
+file = create_output_file(output)
+for y, m in zip(years, months):
+    _, n_days = monthrange(2000 + y, m)
+    days = np.arange(2, n_days + 1)
+    print("Processing {}/{}".format(y, m))
+    for d in tqdm.tqdm(days):
+        extract_data(data_path, y, m, d, samples, file)
+file.close()
