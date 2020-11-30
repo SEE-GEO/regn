0feafa17a76749d03b039a70e68486a57fce8966
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-11-30 16:56:28 +0100

    Added matplotlib style file.

--- regn/__init__.py
+++ regn/__init__.py
@@ -1,2 +1,27 @@
 from pathlib import Path
 regn_path = Path(__file__).parent.parent
+from urllib.request import urlretrieve
+
+def download_data():
+
+    datasets = ["training_data_gmi_small.nc",
+                "training_data_gmi_large.nc",
+                "test_data_gmi_small.nc",
+                "validation_data_gmi_small.nc"]
+
+    for file in datasets:
+        file_path = regn_path / "data" / file
+        if not file_path.exists():
+            print(f"Downloading file {file}.")
+            url = f"http://spfrnd.de/data/regn/{file}"
+            urlretrieve(url, file_path)
+
+    models = ["qrnn_gmi.pt"]
+    for file in models:
+        file_path = regn_path / "models" / file
+        if not file_path.exists():
+            print(f"Downloading file {file}.")
+            url = f"http://spfrnd.de/data/regn/{file}"
+            urlretrieve(url, file_path)
+
+download_data()
