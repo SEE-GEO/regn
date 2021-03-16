04fbf041dbfe20b448b53a3111ce6c4c2feaef0e
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-03-16 11:23:34 +0100

    Added setup_vera.sh

---
+++ scripts/setup_vera.sh
@@ -0,0 +1,4 @@
+export PYTHONPATH=${HOME}:${HOME}/src/scikit-image:${HOME}/src/regn:${HOME}/src/typhon:${PYTHONPATH}
+module load iccifort/2019.1.144-GCC-8.2.0-2.31.1 impi/2018.4.274 GCC/8.2.0-2.31.1  CUDA/10.1.105  OpenMPI/3.1.3 PyTorch/1.2.0-Python-3.7.2 cURL/7.63.0 netcdf4-python/1.5.2 xarray/0.13.0-Python-3.7.2 matplotlib/3.0.3-Python-3.7.2 numba/0.43.1 scikit-learn/0.20.3-Python-3.7.2 tqdm/4.32.1
+
+
